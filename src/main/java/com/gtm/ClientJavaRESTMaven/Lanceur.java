2a7cce85c5fde22b96b0d9b591fb9ad9fd6f771f
izardj <izard.jerome@free.fr>
2017-05-05 11:59:22 +0200

    Création d'un client JAVA pour un service REST

---
+++ src/main/java/com/gtm/ClientJavaRESTMaven/Lanceur.java
@@ -0,0 +1,26 @@
+package com.gtm.ClientJavaRESTMaven;
+
+
+import java.net.URI;
+
+import javax.ws.rs.core.UriBuilder;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
+
+public class Lanceur {
+
+	public static void main(String[] args) {
+		ClientConfig config = new DefaultClientConfig();
+		Client client = Client.create(config);
+		
+		URI uri = UriBuilder.fromUri("http://localhost:8085/TPWSREST").build();
+		WebResource service = client.resource(uri);
+		WebResource path = service.path("banque").path("infos");
+		String res = path.get(String.class);
+		System.out.println(res);
+	}
+
+}
